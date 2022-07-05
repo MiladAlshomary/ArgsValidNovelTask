d0db190478965ecf40fe3df30f7a34ffa72d6faf
Milad Alshomary <sile2804@betaweb036.medien.uni-weimar.de>
2022-07-05 15:47:48 +0200

    updates

--- code/src-py/utils.py
+++ code/src-py/utils.py
@@ -11,3 +11,18 @@ def compute_metrics(pred, average='binary'):
         'precision': precision,
         'f1': f1,
     }
+
+def compute_nli_metrics(pred, average='binary'):
+    labels = pred.label_ids
+    preds = pred.predictions.argmax(-1)
+    
+    labels= [1 if x==2 else -1 for x in labels]
+    preds = [1 if x==2 else -1 for x in preds]
+    
+    precision, recall, f1, _ = precision_recall_fscore_support(labels, preds, average=average)
+    return {
+        'recall' : recall,
+        'precision': precision,
+        'f1': f1,
+    }
+
